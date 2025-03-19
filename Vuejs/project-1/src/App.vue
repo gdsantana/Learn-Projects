758e0c0cb78d40d84c6b8b7fe0ca4047789718ec
gdsantana <guilherme30santana@gmail.com>
2025-03-18 21:10:12 -0300

    [Vue] Tela de login

--- Vuejs/project-1/src/App.vue
+++ Vuejs/project-1/src/App.vue
@@ -1,30 +1,22 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="home-container">
+    <Login />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+
+<script setup lang="ts">
+import Login from './components/Login.vue'
+</script>
+
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.home-container {
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background-color: #222222;
+  
 }
 </style>
---
+++ Vuejs/project-1/src/components/Login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="login-container">
+    <h1 class="title">Faça Login</h1>
+
+    <form class="login-form">
+      <input class="input" type="text" placeholder="Usuário" />
+      <input class="input" type="password" placeholder="Senha" />
+      <button type="submit" @click="showWarning">Entrar</button>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+function showWarning() {
+  alert('My bad, não fiz ainda');
+}
+</script>
+
+<style scoped>
+.login-container {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  background-color: grey;
+  padding: 20px;
+  border-radius: 8px;
+  gap: 24px;
+}
+.title{
+  font-size: 32px;
+}
+.login-form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+.input {
+  padding: 10px;
+  border-radius: 8px;
+  border: none;
+  outline: none;
+  color: yellow;
+  background-color: black;
+}
+</style>
